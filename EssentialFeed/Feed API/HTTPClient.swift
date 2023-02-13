6ae1100eb967cfe22c7ed9bcb8717f0dac42302e
Milos Grujic <milos_grujic@epam.com>
2023-02-13 11:36:18 +0100

    Move HTTPClient to its own file

---
+++ EssentialFeed/Feed API/HTTPClient.swift
@@ -0,0 +1,18 @@
+//
+//  HTTPClient.swift
+//  EssentialFeed
+//
+//  Created by Milos Grujic on 13.2.23..
+//
+
+import Foundation
+
+
+public enum HTTPClientResult {
+    case success(HTTPURLResponse, Data)
+    case failure(Error)
+}
+
+public protocol HTTPClient {
+    func get(from url: URL, completion: @escaping (HTTPClientResult) -> Void)
+}
--- EssentialFeed/Feed API/RemoteFeedLoader.swift
+++ EssentialFeed/Feed API/RemoteFeedLoader.swift
@@ -7,15 +7,7 @@
 
 import Foundation
 
-public enum HTTPClientResult {
-    case success(HTTPURLResponse, Data)
-    case failure(Error)
-}
 
-public protocol HTTPClient {
-    func get(from url: URL, completion: @escaping (HTTPClientResult) -> Void)
-}
- 
 public final class RemoteFeedLoader {
     private let url: URL
     private let client: HTTPClient
